f585e28bd22c08079fa162316cf7e862e0cc28ac
icarohenrrique56-gif <icarohenrrique56@gmail.com>
2025-11-12 15:48:22 +0000

    Versão 1.1

--- firebase-init.js
+++ firebase-init.js
@@ -9,13 +9,14 @@
 // ----------------------------------------------------------------
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  databaseURL: "SEU_DATABASE_URL", // Essencial para o Realtime DB
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE_BUCKET",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyBBrp0r27bdq0_Giw-OcefyGTgtNny2U_g",
+  authDomain: "gfyfy7fy.firebaseapp.com",
+  databaseURL: "https://gfyfy7fy-default-rtdb.firebaseio.com",
+  projectId: "gfyfy7fy",
+  storageBucket: "gfyfy7fy.firebasestorage.app",
+  messagingSenderId: "55023990965",
+  appId: "1:55023990965:web:227802c9e20ba07f6f26fc",
+  measurementId: "G-WK5QQDSC4Y"
 };
 
 // Inicializa o Firebase
